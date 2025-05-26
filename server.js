66dfa5de95f312e20a0c935628d2969e7ce8ecb5
katsuhiro-eqol <katsuhiro.yamashita.sr01@gmail.com>
2025-05-26 11:46:09 +0900

    20250525

--- server.js
+++ server.js
@@ -9,9 +9,7 @@ const httpServer = createServer(app);
 
 // CORSの設定
 app.use(cors({
-  origin: process.env.NODE_ENV === 'production'
-    ? [process.env.CLIENT_URL]  // クライアントのURL
-    : ["http://localhost:3000"],
+  origin: [process.env.CLIENT_URL, "http://localhost:3000"],
   methods: ["GET", "POST"],
   credentials: true
 }));
@@ -19,9 +17,7 @@ app.use(cors({
 // Socket.IOの設定
 const io = new Server(httpServer, {
   cors: {
-    origin: process.env.NODE_ENV === 'production'
-      ? [process.env.CLIENT_URL]
-      : ["http://localhost:3000"],
+    origin:  [process.env.CLIENT_URL, "http://localhost:3000"],
     methods: ["GET", "POST"],
     credentials: true
   }
